397752614fc8362cc70b5dab630e007dbe258353
Lokananda Dhage Munisamappa <lokananda@Lokanandas-MacBook-Pro.local>
2017-04-16 00:04:33 -0500

    test batch/non batch mode of active learner

--- activelearning/tests/test_active_learner.py
+++ activelearning/tests/test_active_learner.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 from activelearning.exampleselector.entropy_based_example_selector import EntropyBasedExampleSelector
 from activelearning.labeler.cli_labeler import CliLabeler
 from activelearning.activelearner.active_learner import ActiveLearner
-
+from activelearning.utils.validation import validate_attr
 class ActiveLearnerTests(unittest.TestCase):
     
     def sample_get_instruction_fn(self, context):
@@ -27,26 +27,33 @@ class ActiveLearnerTests(unittest.TestCase):
     def setUp(self):
         dataset_a=pd.read_csv(os.path.join(os.path.dirname(__file__), 'Data/table_A.csv')).head(1000)
         dataset_b=pd.read_csv(os.path.join(os.path.dirname(__file__), 'Data/table_B.csv')).head(1000)
-        context = {"dataset_a": dataset_a, "dataset_b": dataset_b }
+        self.context = {"dataset_a": dataset_a, "dataset_b": dataset_b }
         # labeled data, typically small in number in DataFrame format
-        labeled_dataset_seed = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Data/seed.csv'),  sep='\t')
+        self.labeled_dataset_seed = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Data/seed.csv'),  sep='\t')
     
         self.unlabeled_dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Data/sample_fvs.csv'), sep='\t')
 
         #create a model
-        model = RandomForestClassifier()   
+        self.model = RandomForestClassifier()   
         #create a labeler
         #Initialize the EMCliLabeler
-        labeler = CliLabeler(self.sample_get_instruction_fn, self.get_example_display_fn, {'y': 1, 'n': 0})
+        self.labeler = CliLabeler(self.sample_get_instruction_fn, self.get_example_display_fn, {'y': 1, 'n': 0})
     
         #create a selector
-        selector  = EntropyBasedExampleSelector()
-        #create a learner
-        alearner = ActiveLearner(model, selector, labeler, 1, 1)
-        alearner.learn(self.unlabeled_dataset, labeled_dataset_seed, exclude_attrs=['_id', 'l_ID', 'r_ID'], context=context, label_attr='label')
+        self.selector  = EntropyBasedExampleSelector()
+        
 
     #testing non batch mode
-    def test_learn(self):
+    def test_active_learn_non_batch(self):
+        #create a learner
+        alearner = ActiveLearner(self.model, self.selector, self.labeler, 1, 2)
+        alearner.learn(self.unlabeled_dataset, self.labeled_dataset_seed, exclude_attrs=['_id', 'l_ID', 'r_ID'], context=self.context, label_attr='label')
         assert_equal(0,0)
-        
+    
+    #testing non batch mode
+    def test_active_learn_batch(self):
+        #create a learner
+        alearner = ActiveLearner(self.model, self.selector, self.labeler, 2, 2)
+        alearner.learn(self.unlabeled_dataset, self.labeled_dataset_seed, exclude_attrs=['_id', 'l_ID', 'r_ID'], context=self.context, label_attr='label')
+        assert_equal(0,0)    
         
