d2aacf7bf7645597bb388025cb3f2a2c826a1628
Lokananda Dhage Munisamappa <lokananda@Lokanandas-MacBook-Pro.local>
2017-04-29 21:20:51 -0500

    refactoring the example selector

--- activelearn/exampleselector/entropy_based_example_selector.py
+++ activelearn/exampleselector/entropy_based_example_selector.py
@@ -16,6 +16,8 @@ class EntropyBasedExampleSelector(UncertaintyBasedExampleSelector):
     def __init__(self):
         super(EntropyBasedExampleSelector, self).__init__()
     
+    def _utility_example(self, probability):
+        self._compute_entropy(probability)
     
     def _compute_entropy(self, probability):
         if 0 in probability:
@@ -23,8 +25,7 @@ class EntropyBasedExampleSelector(UncertaintyBasedExampleSelector):
         else:
             return pd.np.sum(-probability * pd.np.log(probability))
     
-    def select_examples(self, unlabeled_dataset, model, exclude_attrs=None, 
-                        batch_size=1):
+    def select_uncertain_examples(self, probabilities, unlabeled_dataset, batch_size):
         """
         Used to select informative examples based on the entropy of the examples.
 
@@ -33,23 +34,11 @@ class EntropyBasedExampleSelector(UncertaintyBasedExampleSelector):
             unlabeled_dataset (Pandas DataFrame): A Dataframe containing unlabeled examples
             exclude_attrs (list): Attributes which are not feature attributes.
             batch_size (number): The number of examples to select
+        
         Returns:
-            The informative examples
+            The table of most informative examples to be labeled (DataFrame)
         """
-        # check if the input candset is a dataframe
-        validate_input_table(unlabeled_dataset, 'unlabeled dataset')
-        
-        #validate exclude attr
-        for attr in exclude_attrs:
-            validate_attr(attr, unlabeled_dataset.columns, "attr", 'unlabeled_dataset')
-        
-        # remove exclude attrs
-        feature_attrs = remove_exclude_attr(list(unlabeled_dataset.columns), exclude_attrs, unlabeled_dataset)
 
-        feature_values = unlabeled_dataset[feature_attrs]
-        
-        # compute the prediction probabilities for the unlabeled dataset
-        probabilities = model.predict_proba(feature_values) 
 
         entropies = {} 
         # compute the entropy for the unlabeled pairs
--- activelearn/exampleselector/uncertainty_based_example_selector.py
+++ activelearn/exampleselector/uncertainty_based_example_selector.py
@@ -1,5 +1,8 @@
 
 from activelearn.exampleselector.example_selector import ExampleSelector
+from activelearn.utils.validation import validate_input_table
+from activelearn.utils.validation import validate_attr
+from activelearn.utils.helper_functions import remove_exclude_attr
 
 class UncertaintyBasedExampleSelector(ExampleSelector):
     """
@@ -10,3 +13,27 @@ class UncertaintyBasedExampleSelector(ExampleSelector):
         
     def _utility_example(self):
         raise NotImplementedError( "Should have implemented this" )
+
+    def _validate_tables_and_calc_probabilities(self, unlabeled_dataset, model, exclude_attrs=None):
+                # check if the input candset is a dataframe
+        validate_input_table(unlabeled_dataset, 'unlabeled dataset')
+        
+        #validate exclude attr
+        for attr in exclude_attrs:
+            validate_attr(attr, unlabeled_dataset.columns, "attr", 'unlabeled_dataset')
+        
+        # remove exclude attrs
+        feature_attrs = remove_exclude_attr(list(unlabeled_dataset.columns), exclude_attrs, unlabeled_dataset)
+
+        feature_values = unlabeled_dataset[feature_attrs]
+        
+        # compute the prediction probabilities for the unlabeled dataset
+        probabilities = model.predict_proba(feature_values)
+        
+        return probabilities
+    
+    def select_examples(self, unlabeled_dataset, model, exclude_attrs=None, 
+                        batch_size=1):
+        probabilities = self._validate_tables_and_calc_probabilities(unlabeled_dataset, model, exclude_attrs)
+        return self.select_uncertain_examples(probabilities, unlabeled_dataset, batch_size)
+        
